289ef8015743c9eeb4412a8b44d3cefd2fb275f1
hlxll <447404195@qq.com>
2023-03-09 20:15:57 +0800

    更新

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+var jwt = require("jsonwebtoken");
 // 实现jsonp接口
 router.get("/jsonp", function (req, res, next) {
   // 获取客户端发送过来的回调函数名字
@@ -12,5 +12,13 @@ router.get("/jsonp", function (req, res, next) {
   // 返回给客户端，用script解析
   res.send(scriptstr);
 });
-
+router.get("/setjwt", function (req, res) {
+  var secretKey = "slakjdlskjhkdj";
+  res.send({
+    message: "",
+    status: 200,
+    token: jwt.sign({ username: "huanglin" }, secretKey, { expiresIn: "30h" }),
+  });
+  // sign参数：用户信息，加密密钥，配置信息
+});
 module.exports = router;
